9dedb2eac48554d2279101b1a235c8bc36913afe
Naveen M K <naveen@syrusdark.website>
2021-05-30 15:44:03 +0530

    Basic Tests

--- tests/test_manim_fonts.py
+++ tests/test_manim_fonts.py
@@ -1,5 +1,30 @@
-from manim_fonts import __version__
+import json
+from pathlib import Path
+
+import appdirs
+import pytest
 
 
-def test_version():
-    assert __version__ == "0.1.0"
+@pytest.fixture
+def new_fonts_dir(tmpdir, monkeypatch):
+    tmpdir = Path(tmpdir)
+
+    def tmp(*args):
+        return tmpdir
+
+    monkeypatch.setattr(appdirs, "user_cache_dir", tmp)
+    yield tmpdir
+
+
+def test_download(new_fonts_dir):
+
+    from manim_fonts import RegisterFont
+    from manim_fonts.config import FONTS_DIR
+
+    assert FONTS_DIR == new_fonts_dir / "fonts"
+    with RegisterFont("Odibee Sans") as f:
+        assert f
+        assert Path(FONTS_DIR).exists()
+        assert list(Path(FONTS_DIR).glob("*"))
+        assert Path(FONTS_DIR) / "fonts.json"
+        assert json.load((Path(FONTS_DIR) / "fonts.json").open())
