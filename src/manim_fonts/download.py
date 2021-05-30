83df9d8b4daa0d65235acc8855bf106502b537f7
Naveen M K <naveen@syrusdark.website>
2021-05-30 15:16:52 +0530

    Retry while downloading and use chunk method

--- src/manim_fonts/download.py
+++ src/manim_fonts/download.py
@@ -1,5 +1,6 @@
 import shutil
 import tempfile
+import time
 import zipfile
 from pathlib import Path
 
@@ -15,6 +16,30 @@ from .utils import (
 )
 
 
+def download(url: str, local_filename: Path):
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, "wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+
+
+def download_and_retry(url: str, local_filename: Path):
+    logger.info("Downloading %s to %s", url, local_filename)
+    for i in range(3):
+        logger.info("Try: %s/3", i + 1)
+        try:
+            download(url, local_filename)
+            break
+        except (requests.HTTPError, requests.ConnectionError) as e:
+            time.sleep(1)
+            logger.debug(e)
+
+    else:
+        raise requests.HTTPError("%s can't be downloaded" % url)
+    return True
+
+
 def download_fonts(font_family: str) -> Path:
     font_url = gen_fonts_google_url(font_family)
     font_hash = str2hash(font_family)
@@ -22,12 +47,9 @@ def download_fonts(font_family: str) -> Path:
     font_location.mkdir(parents=True, exist_ok=True)
     if font_hash not in get_downloaded_fonts():
         logger.info("Downloading %s from %s", font_family, font_url)
-        con = requests.get(font_url)
-        con.raise_for_status()
         with tempfile.TemporaryDirectory(prefix="manim-fonts") as tmpdir:
             tfile = Path(tmpdir) / "temp.zip"
-            with open(tfile, "wb") as f:
-                f.write(con.content)
+            download_and_retry(font_url, tfile)
             with zipfile.ZipFile(tfile, "r") as zip:
                 zip.extractall(tmpdir)
             for file in Path(tmpdir).glob("*.ttf"):
